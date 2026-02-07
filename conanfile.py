47cad6a7fe735112e9ab0e7470bda087091ea0a9
Brian Szmyd <bszmyd@ebay.com>
2026-02-06 21:56:32 -0700

    SDSTOR-20907: Update for NuBlox2.

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,13 @@
-from os.path import join
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
 from conan.tools.files import copy
-from conans import CMake
+from os.path import join
 
 class IOMgrConan(ConanFile):
     name = "iomgr"
-    version = "8.8.2"
+    version = "8.8.3"
     homepage = "https://github.com/eBay/IOManager"
     description = "Asynchronous event manager"
     topics = ("ebay", "nublox", "aio")
@@ -31,61 +33,78 @@ class IOMgrConan(ConanFile):
         "pistache:with_ssl": True,
     }
 
-    generators = "cmake", "cmake_find_package"
     exports_sources = "CMakeLists.txt", "cmake/*", "src/*", "test/*", "LICENSE"
 
     def configure(self):
         if self.options.shared:
             del self.options.fPIC
         if self.settings.build_type == "Debug":
-            if self.options.sanitize:
-                self.options['sisl'].sanitize = True
             if self.options.coverage and self.options.sanitize:
                 raise ConanInvalidConfiguration("Sanitizer does not work with Code Coverage!")
         if self.settings.arch != "x86_64":
             self.options["spdk"].native_build = True
 
     def build_requirements(self):
-        self.build_requires("gtest/1.12.1")
+        self.test_requires("gtest/1.15.0")
 
     def requirements(self):
-        self.requires("sisl/[~=8]")
+        self.requires("sisl/8.9.5")
 
         self.requires("boost/1.79.0")
         self.requires("folly/2022.01.31.00")
-        self.requires("grpc/1.50.1")
+        self.requires("grpc/1.54.3")
         self.requires("grpc_internal/1.48.0")
         self.requires("liburing/2.1")
-        self.requires("nlohmann_json/3.11.2")
+        self.requires("nlohmann_json/3.12.0")
         self.requires("libevent/2.1.12")
         self.requires("spdk/nbi.21.07.y")
         self.requires("pistache/nbi.0.0.5.1")
 
-        self.requires("openssl/1.1.1s",     override=True)
+        self.requires("openssl/3.6.0", override=True)
+
+    def layout(self):
+        self.folders.source = "."
+        if self.options.get_safe("sanitize"):
+            self.folders.build = join("build", "Sanitized")
+        elif self.options.get_safe("coverage"):
+            self.folders.build = join("build", "Coverage")
+        else:
+            self.folders.build = join("build", str(self.settings.build_type))
+        self.folders.generators = join(self.folders.build, "generators")
+
+        self.cpp.source.includedirs = ["src/include"]
+
+        self.cpp.build.libdirs = ["src/lib"]
+        self.cpp.build.includedirs = ["src/include"]
+
+        self.cpp.package.libs = ["iomgr"]
+        self.cpp.package.includedirs = ["include"] # includedirs is already set to 'include' by
+        self.cpp.package.libdirs = ["lib"]
+
+    def generate(self):
+        # This generates "conan_toolchain.cmake" in self.generators_folder
+        tc = CMakeToolchain(self)
+        tc.variables['CMAKE_TEST_TARGET'] = self.options.testing
+        tc.variables['CMAKE_EXPORT_COMPILE_COMMANDS'] = 'ON'
+        tc.variables["CTEST_OUTPUT_ON_FAILURE"] = "ON"
+        tc.variables["MEMORY_SANITIZER_ON"] = "OFF"
+        tc.variables["BUILD_COVERAGE"] = "OFF"
+        if self.settings.build_type == "Debug":
+            if self.options.get_safe("coverage"):
+                tc.variables['BUILD_COVERAGE'] = 'ON'
+            elif self.options.get_safe("sanitize"):
+                tc.variables['MEMORY_SANITIZER_ON'] = 'ON'
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        definitions = {'CMAKE_TEST_TARGET': self.options.testing,
-                       'CONAN_CMAKE_SILENT_OUTPUT': 'ON',
-                       'CMAKE_EXPORT_COMPILE_COMMANDS': 'ON',
-                       'MEMORY_SANITIZER_ON': 'OFF'}
-        test_target = None
-
-        run_tests = True
-        if self.settings.build_type == "Debug":
-            if self.options.sanitize:
-                definitions['MEMORY_SANITIZER_ON'] = 'ON'
-            elif self.options.coverage:
-                definitions['BUILD_COVERAGE'] = 'ON'
-                test_target = 'coverage'
-            else:
-                run_tests = False
-
-        cmake.configure(defs=definitions)
+        cmake.configure()
         cmake.build()
-        # Only test in Sanitizer mode, Coverage mode or Release mode
-        if run_tests:
-            cmake.test(target=test_target, output_on_failure=True)
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake.test()
 
     def package(self):
         copy(self, "LICENSE", self.source_folder, join(self.package_folder, "licenses"), keep_path=False)
